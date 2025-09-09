32b4050c9608708c44f39ddb48d19794692a5cf5
gigan21 <radarojasc@gmail.com>
2025-09-09 15:34:50 -0400

    svolver

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,7 @@
 <?php
 
-use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware('auth');
-
-
+Route::get('/', function () {
+    return view('welcome');
+});
